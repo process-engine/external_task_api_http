5f190adb2dcf63330b71a3e7b192db238e505d3a
ElRaptorus <christian.werner@5minds.de>
2018-10-11 10:58:00 +0200

    :lipstick: Apply review feedback from contracts

--- src/external_task_api_controller.ts
+++ src/external_task_api_controller.ts
@@ -9,7 +9,6 @@ import {
   HandleBpmnErrorRequestPayload,
   HandleServiceErrorRequestPayload,
   IExternalTaskApi,
-  restSettings,
 } from '@process-engine/external_task_api_contracts';
 
 import {Response} from 'express';
@@ -28,15 +27,18 @@ export class ExternalTaskApiController {
 
   public async fetchAndLockExternalTasks(request: HttpRequestWithIdentity, response: Response): Promise<void> {
 
-    const workerId: string = request.params.worker_id;
     const identity: IIdentity = request.identity;
 
     const payload: FetchAndLockRequestPayload = request.body;
 
-    const result: Array<ExternalTask> =
-      await this
-        .externalTaskApiService
-        .fetchAndLockExternalTasks(identity, workerId, payload.topicName, payload.maxTasks, payload.longPollingTimeout, payload.lockDuration);
+    // Note: The Controller cannot possibly know what type the requesting resource is expecting here.
+    const result: Array<ExternalTask<any>> =
+      await this.externalTaskApiService.fetchAndLockExternalTasks<any>(identity,
+                                                                       payload.workerId,
+                                                                       payload.topicName,
+                                                                       payload.maxTasks,
+                                                                       payload.longPollingTimeout,
+                                                                       payload.lockDuration);
 
     response.status(this.httpCodeSuccessfulResponse).json(result);
   }
@@ -44,12 +46,11 @@ export class ExternalTaskApiController {
   public async extendLock(request: HttpRequestWithIdentity, response: Response): Promise<void> {
 
     const externalTaskId: string = request.params.external_task_id;
-    const workerId: string = request.params.worker_id;
     const identity: IIdentity = request.identity;
 
     const payload: ExtendLockRequestPayload = request.body;
 
-    await this.externalTaskApiService.extendLock(identity, workerId, externalTaskId, payload.additionalDuration);
+    await this.externalTaskApiService.extendLock(identity, payload.workerId, externalTaskId, payload.additionalDuration);
 
     response.status(this.httpCodeSuccessfulNoContentResponse).send();
   }
@@ -57,12 +58,11 @@ export class ExternalTaskApiController {
   public async handleBpmnError(request: HttpRequestWithIdentity, response: Response): Promise<void> {
 
     const externalTaskId: string = request.params.external_task_id;
-    const workerId: string = request.params.worker_id;
     const identity: IIdentity = request.identity;
 
     const payload: HandleBpmnErrorRequestPayload = request.body;
 
-    await this.externalTaskApiService.handleBpmnError(identity, workerId, externalTaskId, payload.errorCode);
+    await this.externalTaskApiService.handleBpmnError(identity, payload.workerId, externalTaskId, payload.errorCode);
 
     response.status(this.httpCodeSuccessfulNoContentResponse).send();
   }
@@ -70,12 +70,11 @@ export class ExternalTaskApiController {
   public async handleServiceError(request: HttpRequestWithIdentity, response: Response): Promise<void> {
 
     const externalTaskId: string = request.params.external_task_id;
-    const workerId: string = request.params.worker_id;
     const identity: IIdentity = request.identity;
 
     const payload: HandleServiceErrorRequestPayload = request.body;
 
-    await this.externalTaskApiService.handleServiceError(identity, workerId, externalTaskId, payload.errorMessage, payload.errorDetails);
+    await this.externalTaskApiService.handleServiceError(identity, payload.workerId, externalTaskId, payload.errorMessage, payload.errorDetails);
 
     response.status(this.httpCodeSuccessfulNoContentResponse).send();
   }
@@ -83,12 +82,12 @@ export class ExternalTaskApiController {
   public async finishExternalTask(request: HttpRequestWithIdentity, response: Response): Promise<void> {
 
     const externalTaskId: string = request.params.external_task_id;
-    const workerId: string = request.params.worker_id;
     const identity: IIdentity = request.identity;
 
-    const payload: FinishExternalTaskRequestPayload = request.body;
+    // Note: The Controller cannot possibly know what type the requesting resource is expecting here.
+    const payload: FinishExternalTaskRequestPayload<any> = request.body;
 
-    await this.externalTaskApiService.finishExternalTask(identity, workerId, externalTaskId, payload.result);
+    await this.externalTaskApiService.finishExternalTask<any>(identity, payload.workerId, externalTaskId, payload.result);
 
     response.status(this.httpCodeSuccessfulNoContentResponse).send();
   }
